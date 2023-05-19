f806619a49d1f8c4eb5cee5bd26706d0478839e2
Asnoyon <blackpain1100@gmail.com>
2023-05-19 11:56:00 +0600

    react update

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import {Greet} from './components/greet';
 import Welcome from './components/welcome';
+import Hello from './components/Hello';
 function App() {
   return (
     <div className="App">
       <Greet/>
       <Welcome/>
+      <Hello/>
     </div>
   );
 }
