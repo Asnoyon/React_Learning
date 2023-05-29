0ddc9fdd2ccdbd59337f80236816430bbd6df1ff
Asnoyon <blackpain1100@gmail.com>
2023-05-29 21:47:16 +0600

    keyboard add

---
+++ src/Pages/Keyboard.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Keyboard() {
+    const onKeyDownText = ()=>{
+        console.log("key down")
+    }
+    const onKeyUpText = ()=>{
+        console.log("key Up")
+    }
+    const onKeyPressdText = ()=>{
+        console.log("key pressed")
+    }
+  return (
+    <div>
+        <h1>Keyboard Event</h1>
+        <input type="text" onKeyDown={onKeyDownText} />
+        <input type="text" onKeyUp={onKeyUpText} />
+        <input type="text" onKeyPress={onKeyPressdText} />
+    </div>
+  )
+}
+
+export default Keyboard
