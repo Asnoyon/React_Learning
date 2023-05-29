b1e9dbb523065316fcc2aa37994f15dc356952c4
Asnoyon <blackpain1100@gmail.com>
2023-05-30 00:00:50 +0600

    styleJsx update

--- src/Pages/Style.jsx
+++ src/Pages/Style.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
-
+import './Style.css'
 function Style() {
     const styleH1={
         color:"white",
         backgroundColor:"green",
         textAlign: "center",
         padding:"20px",
-        margin: "5px 0"
+       
     }
   return (
     <div>
         <h1 style={styleH1}>My first style Component</h1>
+        <h2 className='noyon'> Hello external Css. How are you</h2>
     </div>
   )
 }
