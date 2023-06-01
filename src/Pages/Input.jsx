d916d3bf8e8b4dd24d9a6ac29e45170a4ad8f0d8
Asnoyon <blackpain1100@gmail.com>
2023-06-01 21:01:17 +0600

    form processing react

--- src/Pages/Input.jsx
+++ src/Pages/Input.jsx
@@ -1,14 +1,23 @@
 import React, { Component } from 'react'
 
 export default class Input extends Component {
+
+    state={
+      ChannelName : "",
+
+    }
     FormChange = (event)=>{
         console.log(event.target.value)
+    }
+    CheckboxHandler = (event)=>{
+      this.setState({
+        checkbox : event.target.checked
+      })
     }
     
   render() {
     return (
       <div className='container'>
-         <input type="text" name='ChannelName' placeholder='Channel Name'  className='form-control' onChange={this.FormChange} />
          <input type="text" name='ChannelName' placeholder='Channel Name'  className='form-control' onChange={this.FormChange} />
          <select name="category" onChange={this.FormChange} className='form-control'>
             <option value="Select">Select</option>
@@ -18,10 +27,16 @@ export default class Input extends Component {
          </select>
          <input type="date" name="CreationDate" onChange={this.FormChange} className='form-control' />
          <textarea name="About Textarea" onChange={this.FormChange} className='form-control'></textarea>
-         <input type="radio" value="male" name="gender" onChange={this.FormChange} />
-         <label htmlFor="gender">Male</label>
-         <input type="radio" value="female" name="gender" onChange={this.FormChange} />
-         <label htmlFor="gender">Female</label>
+         <div>
+          <input type="radio" value="male" name="gender" onChange={this.FormChange} />
+          <label htmlFor="gender">Male</label>
+          <input type="radio" value="female" name="gender" onChange={this.FormChange} />
+          <label htmlFor="gender">Female</label>
+          </div>
+          <input type="checkbox" name="Checkbox" onChange={this.CheckboxHandler} /> I agree and continue <br />
+          <button className='btn btn-primary'>
+            Submit
+          </button>
       </div>
     )
   }
