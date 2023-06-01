351bf33ee8f098ffc21947da4c607ee579e36925
Asnoyon <blackpain1100@gmail.com>
2023-06-01 20:27:05 +0600

    update input

--- src/Pages/Input.jsx
+++ src/Pages/Input.jsx
@@ -9,6 +9,14 @@ export default class Input extends Component {
     return (
       <div className='container'>
          <input type="text" name='ChannelName' placeholder='Channel Name'  className='form-control' onChange={this.FormChange} />
+         <input type="text" name='ChannelName' placeholder='Channel Name'  className='form-control' onChange={this.FormChange} />
+         <select name="category" onChange={this.FormChange} className='form-control'>
+            <option value="Select">Select</option>
+            <option value="Programming">Programming</option>
+            <option value="Education">Education</option>
+            <option value="Entertainment">Entertainment</option>
+         </select>
+         <input type="date" name="CreationDate" onChange={this.FormChange} className='form-control' />
       </div>
     )
   }
