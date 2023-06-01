a7138f77b857ba3bc97147c64ede25aa8cbd18c0
Asnoyon <blackpain1100@gmail.com>
2023-06-01 22:16:51 +0600

    update control

---
+++ src/Pages/ControlComponent.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function ControlComponent() {
+    function handler(){
+        console.log("here")
+    }
+  return (
+    <div className='container'> <br />
+        <input type="text" value="" placeholder='Uncontrolled input' onChange={handler} />
+        <h1>This is JavaScript Expression {Math.random()*100}</h1>
+    </div>
+  )
+}
+
+export default ControlComponent
