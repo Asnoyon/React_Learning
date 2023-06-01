8ad333b347d62b94d035465ea0a294ac3763307e
Asnoyon <blackpain1100@gmail.com>
2023-06-01 21:37:00 +0600

    form done

--- src/Pages/Input.jsx
+++ src/Pages/Input.jsx
@@ -4,37 +4,47 @@ export default class Input extends Component {
 
     state={
       ChannelName : "",
+      category : "",
+      CreationDate: "",
+      AboutTextarea:"",
+      gender:"",
+      Checkbox:false
+
 
     }
     FormChange = (event)=>{
-        console.log(event.target.value)
+        this.setState({
+          [event.target.name]:event.target.value
+        })
     }
     CheckboxHandler = (event)=>{
       this.setState({
-        checkbox : event.target.checked
+        Checkbox : event.target.checked
       })
     }
     
   render() {
+    const{ChannelName,category,CreationDate,AboutTextarea,Checkbox}=this.state
     return (
       <div className='container'>
-         <input type="text" name='ChannelName' placeholder='Channel Name'  className='form-control' onChange={this.FormChange} />
-         <select name="category" onChange={this.FormChange} className='form-control'>
+       
+         <input type="text" name='ChannelName' placeholder='Channel Name'  className='form-control' onChange={this.FormChange} value={ChannelName} />
+         <select name="category" onChange={this.FormChange} value={category} className='form-control'>
             <option value="Select">Select</option>
             <option value="Programming">Programming</option>
             <option value="Education">Education</option>
             <option value="Entertainment">Entertainment</option>
          </select>
-         <input type="date" name="CreationDate" onChange={this.FormChange} className='form-control' />
-         <textarea name="About Textarea" onChange={this.FormChange} className='form-control'></textarea>
+         <input type="date" name="CreationDate" onChange={this.FormChange} value={CreationDate} className='form-control' />
+         <textarea name="AboutTextarea" onChange={this.FormChange} value={AboutTextarea} className='form-control'></textarea>
          <div>
           <input type="radio" value="male" name="gender" onChange={this.FormChange} />
           <label htmlFor="gender">Male</label>
           <input type="radio" value="female" name="gender" onChange={this.FormChange} />
           <label htmlFor="gender">Female</label>
           </div>
-          <input type="checkbox" name="Checkbox" onChange={this.CheckboxHandler} /> I agree and continue <br />
-          <button className='btn btn-primary'>
+          <input type="checkbox" name="Checkbox" checked={Checkbox} onChange={this.CheckboxHandler} /> I agree and continue <br />
+          <button className='btn btn-primary' onClick={()=>{console.log(this.state)}} >
             Submit
           </button>
       </div>
