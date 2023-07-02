5ace0059072073ba1369cae220db684cedcca359
Asnoyon <blackpain1100@gmail.com>
2023-07-02 18:01:04 +0600

    home update

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import About from "./About";
 
-const AboutMe= "My name is Noyon"
+const AboutMee= "My name is Noyon"
 function Home (){
     return (
         <div>
             <h1>Header</h1>
             <h1>Sidebar</h1>
             <h1>Footer</h1>
-            <h1>{AboutMe}</h1>
+            <h1>{AboutMee}</h1>
         </div>
     )
 }
