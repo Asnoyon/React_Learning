a92c90db8971a5abae919006cc9555bbdf897b04
Asnoyon <blackpain1100@gmail.com>
2023-07-06 21:51:19 +0600

    keyboard update

--- src/Pages/Keyboard.jsx
+++ src/Pages/Keyboard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Keyboard() {
     const onKeyDownText = ()=>{
-        console.log("key down")
+        console.log("key Down")
     }
     const onKeyUpText = ()=>{
         console.log("key Up")
